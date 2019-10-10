3f4e816979024fbcbd064dc4f783a2f01a44fe13
davidchuka <bigbochuks@gmail.com>
2019-10-10 11:43:25 +0100

    initial commit :tada:

---
+++ components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <input type="text" v-model="name" placeholder="Enter your name" style="height:40px;width:280px; padding:10px;">
+        <h1>Hello {{name}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return{
+                name: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+h1 {
+    font-size: 4em;
+}
+</style>
